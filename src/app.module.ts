a1d6e7f3ba572ccb879f8ca823193db5920ecd5f
김기주 <kiju@gimgijuui-noteubug.local>
2024-05-18 11:10:27 +0900

    Feat: auth controller 생성/ video module, controller 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { VideoModule } from './video/video.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, VideoModule],
   controllers: [AppController],
   providers: [AppService],
 })
